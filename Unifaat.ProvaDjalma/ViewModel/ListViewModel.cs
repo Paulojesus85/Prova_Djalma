231c0cc0862ef5dd17fd4242392fbb1493f80313
Prof. Paulo Jesus <paulo.historia.usp@gmail.com>
2023-09-25 22:19:25 -0300

    mudança 4

---
+++ Unifaat.ProvaDjalma/ViewModel/ListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Unifaat.ProvaDjalma.Models;
+
+namespace Unifaat.ProvaDjalma.ViewModel
+{
+    public class ListViewModel
+    {
+        public List<string> NomesVendedores { get; set; }
+        public List<string> NomesClientes { get; set; }
+        public List<string> NomesProdutos { get; set; }
+        public List<string> NomesMarcas { get; set; }
+    }
+}
